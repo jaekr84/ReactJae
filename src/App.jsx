603f4e4579fa161ae6cadc6290034ec642424137
Jae Lee <ljckr1984@gmail.com>
2024-03-24 15:36:26 -0300

    push2

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,21 @@ function App() {
           <Card
             image="https://watcher.guru/news/wp-content/uploads/2021/08/unnamed-2-2.png.webp"
             title="APE NFT 1"
-            description="APE PROYECT"
+            description="APE PROYECT 1 "
             button1Text="+ Info"
             button2Text="Comprar"
           />
           <Card
             image="https://en.ethereumworldnews.com/wp-content/uploads/2022/03/Bored-Ape-NFT-835-Sells-for-a-Shocking-115.png"
             title="APE NFT 2"
-            description="APE PROYECT"
+            description="APE PROYECT 2 "
             button1Text="+ Info"
             button2Text="Comprar"
           />
           <Card
             image="https://nftcalendar.io/storage/uploads/events/2021/12/QL1sTupzJLvCo461lINhnJrQbdZfEUXAofpZ9iVW.gif"
             title="APE NFT 3"
-            description="APE PROYECT"
+            description="APE PROYECT 3 "
             button1Text="+ Info"
             button2Text="Comprar"
           />
